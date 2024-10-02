3da9eca7728db5d060f0841091b5cef67f61d414
victor loginov <tkoe88@zoho.com>
2024-10-02 23:34:13 +0200

    set up some basic express routes

--- 02-ExpressTrain/app.ts
+++ 02-ExpressTrain/app.ts
@@ -1,20 +1,36 @@
-import http from 'http';
 import express from 'express';
+
+const form = `
+<form action='/station' method='post'>
+  <label for='station'>The Express Train 🚂</label>
+  <input id='station' name='station' />
+  <button>All Aboard</button>
+<form/>`;
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('middleware one');
-  next();
+// replaces bodyparser.urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/express-train', (req, res, next) => {
+  res.send(form);
 });
 
-app.use((req, res, next) => {
-  console.log('middleware two');
-  res.send('<h1>all aboard the express train 🚂</h1>')
+app.post('/station', (req, res, next) => {
+  console.log(req.body);
+  // res.redirect('/');
+  const station = req.body.station;
+  res.redirect(`/?station=${station}`); // Attach 'station' as a query parameter and redirect
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  const { station } = req.query;
+  res.send(
+    `<h1>${station ? station + '🏭' : 'You are Home 🏡'}</h1><a href='/express-train'>Take the Express</a>`
+  );
+});
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+// express's app.listen consumes of http.createServer(app); & server.listen()
+app.listen(3000, () => {
+  console.log('Server is on track to port 3000');
 });
