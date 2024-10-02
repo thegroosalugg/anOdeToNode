6793fe17f764b1719a7b9787766a7de8dabef5ed
victor loginov <tkoe88@zoho.com>
2024-10-02 18:29:03 +0200

    set up express

--- 02-ExpressTrain/app.ts
+++ 02-ExpressTrain/app.ts
@@ -1,8 +1,19 @@
 import http from 'http';
+import express from 'express';
 
-const server = http.createServer((req, res) => {
-  res.end('all aboard the express train 🚂')
-})
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('middleware one');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('middleware two');
+  res.send('<h1>all aboard the express train 🚂</h1>')
+});
+
+const server = http.createServer(app);
 
 server.listen(3000, () => {
   console.log('Server is listening on port 3000');
