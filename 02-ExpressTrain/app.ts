c3db89c558db0ba2616886a42d4b15daafe95dc3
victor loginov <tkoe88@zoho.com>
2024-10-08 23:45:10 +0200

    can write data to json

--- 02-ExpressTrain/app.ts
+++ 02-ExpressTrain/app.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import adminRoutes from './routes/admin';
 import stationRoutes from './routes/station';
@@ -7,8 +8,8 @@ console.clear();
 
 const app = express();
 
-// replaces bodyparser.urlencoded
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // replaces bodyparser.urlencoded
+app.use(express.static(path.join(__dirname, 'public'))); // allows serving of static paths
 
 app.use('/express', adminRoutes); // adds URL filter to all routes
 app.use(stationRoutes);
--- 02-ExpressTrain/models/station.ts
+++ 02-ExpressTrain/models/station.ts
@@ -1,5 +1,5 @@
-const stations: Station[] = [];
-
+import fs from 'fs';
+import path from 'path';
 class Station {
   name: string;
 
@@ -8,12 +8,31 @@ class Station {
   }
 
   save() {
-    stations.push(this);
+    // finds the location of the file we want to read
+    // path.join writes the directory of the file's location in the project folder structure
+    // path.dirname(require.main.filename) returns the path of the main module that started the application
+    const filePath = path.join(
+      path.dirname(require?.main?.filename || process.cwd()), // app/ts root dir
+      'data', // data folder, sibling of app.ts
+      'stations.json' // filename we want to read
+    );
+    fs.readFile(filePath, 'utf8', (err, data) => { // utf8 converts data Buffer to string formate so it can be parsed
+      let stations = []; // create file if it doesn't exist
+      if (!err) {
+        // if no error when reading file, reasign stations to parsed json content
+        stations = JSON.parse(data);
+      }
+      stations.push(this); // save new class instance to array
+      fs.writeFile(filePath, JSON.stringify(stations), (err) => {
+        // write file to JSON as string. Either existing readFile, or save new declared with 'let'
+        console.log(err);
+      });
+    });
   }
 
   // static allows function to be called on the Model itself, rathen than an object instance
   static fetchAll() {
-    return stations;
+    return [];
   }
 }
 
