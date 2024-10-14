b6a18ab883a5f75e315284382b6697dbecc150b9
victor loginov <tkoe88@zoho.com>
2024-10-14 20:25:12 +0200

    updated form data saving function

--- 02-ExpressTrain/controllers/admin.ts
+++ 02-ExpressTrain/controllers/admin.ts
@@ -10,12 +10,13 @@ const getAddItem: RequestHandler = (req, res, next) => {
 
 // /admin/add-item
 const postAddItem: RequestHandler = (req, res, next) => {
-  const name  =  req.body.name.trim();
-  const desc  =  req.body.description.trim();
-  const price = +req.body.price.trim();
+  const trimBody = Object.fromEntries(
+    Object.entries(req.body).map(([key, value]) => [key, (value as string).replace(/\s+/g, ' ')])
+  );
+  const { name, description, price } = trimBody;
 
-  if (name && desc && price > 0) {
-    const item = new Item(name, desc, '/images/board_red_blue.png', price);
+  if (name && description && +price > 0) {
+    const item = new Item(name, description, '/images/board_red_blue.png', +price);
     item.save();
     res.redirect('/');
   }
