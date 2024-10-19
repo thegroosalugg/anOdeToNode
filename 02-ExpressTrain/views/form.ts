ab797417ff60be22ff6d31ce6593ad5d8b158286
victor loginov <tkoe88@zoho.com>
2024-10-19 12:03:02 +0200

    form styling overhaulted

--- 02-ExpressTrain/views/form.ts
+++ 02-ExpressTrain/views/form.ts
@@ -1,35 +1,72 @@
-import Item from "../models/Item";
+import Item from '../models/Item';
 
 const formCSS = /*css*/ `
   .form {
-    margin: 2rem auto;
+    width: 100%;
+    max-width: 400px;
+    margin: 1rem auto 0;
+    padding: 1rem;
     display: flex;
     flex-direction: column;
-    gap: 0.2rem;
+    gap: 0.5rem;
+    background-image: url('/assets/logo1.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
 
+    > h1 {
+      background-image: url('/assets/inksplash.png');
+      background-size: 100% 100%;
+      height: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      color: #ffffff;
+      font-weight: 500;
+      font-size: 1.5rem;
+    }
 
     > section {
       display: flex;
       flex-direction: column;
 
+      label, input, textarea {
+        background: linear-gradient(to right, rgba(82, 81, 80, 0.85), rgba(234, 234, 234, 0.50));
+        color: #ffffff;
+        text-shadow: 1px 1px 2px #8c8c8c;
+        font-size: 1.2rem;
+        overflow-x: scroll;
+      }
+
       label {
+        padding: 0.2rem;
         text-transform: capitalize;
+        border-radius: 3px 3px 0 0;
       }
 
       input {
-        border: 1px solid #000000;
+        height: 2rem;
       }
 
-      textarea {
+      input, textarea {
+        padding: 0.1rem 0.3rem;
+        text-shadow: 1px 1px 2px #5f5f5f;
+        border: none;
+        border-top: 2px solid #ffffff;
+        border-radius: 0 0 3px 3px;
         resize: none;
       }
     }
 
     button {
       padding: 0.2rem 0.3rem;
+      font-size: 1.2rem;
       background: #1183a0;
       color: #FFFFFF;
       border: 0.5px solid transparent;
+      border-radius: 3px;
+      overflow-x: scroll;
       cursor: pointer;
       transition: 0.5s ease-in-out;
 
@@ -55,15 +92,23 @@ const input = (id: string, value: string, text?: boolean) => /*html*/ `
 
 const form = (item?: Item) => {
   const { name = '', price = '', desc = '' } = item || {};
+  const backgroundImg = `/assets/logo${item ? 1 : 2}.png`;
 
   return /*html*/ `
-    <form action='/admin/add-item' method='post' class='form' >
+    <form action="/admin/add-item" method="post" class="form" style="background-image: url('${backgroundImg}')">
+      <h1>${item ? 'Edit' : 'New'} Listing</h1>
       ${input('name', name)}
       ${input('price', price + '')}
       ${input('description', desc, true)}
-      <button>Add <i class="fa-solid fa-square-plus"></i></button>
+      <button>
+        ${
+          item
+            ? /*html*/ `Update <i class="fa-solid fa-check"></i>`
+            : /*html*/ `Add    <i class="fa-solid fa-plus"></i>`
+        }
+      </button>
     </form>
-  `
+  `;
 };
 
 export { formCSS, form };
