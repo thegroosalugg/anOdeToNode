c0bd7f3e522114918e7ce75d19920e06cde34b95
victor loginov <tkoe88@zoho.com>
2024-11-14 23:11:50 +0100

    moved shared project images to root folder

--- 03-SQL/app.ts
+++ 03-SQL/app.ts
@@ -21,7 +21,7 @@ db.all('SELECT * FROM items', (err, items) => {
 app.use(express.urlencoded({ extended: false })); // replaces bodyparser.urlencoded
 
  // allows serving of static paths
-app.use(express.static(path.join(import.meta.dirname, 'public'), {
+app.use(express.static(path.join(import.meta.dirname, '../', 'public'), {
   maxAge: '1d', // Cache static assets for 1 day to improve load times
   etag: false  // Disable ETag generation for simpler cache management
 }));
