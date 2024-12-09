598bb9cbc882021e388c672615bd69efeb3a5464
victor loginov <tkoe88@zoho.com>
2024-12-09 15:59:09 +0100

    uodated errorMsg function

--- 08-Authentication/util/errorMsg.ts
+++ 08-Authentication/util/errorMsg.ts
@@ -1,14 +1,26 @@
-const stars = (n: number) => '*'.repeat(n);
-const barrier = ()=> console.log(stars(80));
+const col = (n: number) => `\x1b[3${n}m`;
+const end = '\x1b[0m';
+
+const barrier = (s: string, n: number, noLog?: boolean) => {
+  const chars = col(5) + s.repeat(n) + end;
+  if (noLog) return chars;
+  console.log(chars);
+};
 
 const errorMsg = ({ msg, error }: { msg: string; error: any }) => {
-  barrier();
-  console.log(stars(35) + ' <% ERROR %> ' + msg.toUpperCase());
-  barrier();
+  barrier('№', 84);
+  console.log(
+    `${barrier('▣', 28, true)} ${col(1)}<% ERROR %> ${msg.toUpperCase()}${end}`
+  );
+  barrier('№', 84);
   console.log(error);
-  barrier();
-  console.log(stars(38) + ' <% END %> ' + msg.toUpperCase() + ' <% END %>');
-  barrier();
+  barrier('⇎⇏', 42);
+  console.log(
+    `${barrier('∜', 28, true)} ${col(2)}<% END %> ${msg.toUpperCase()} <% END %>${end}`
+  );
+  barrier('⇏⇎', 42);
 };
 
 export default errorMsg;
+
+// ▣⇄⇎⇏▭◊№⁕⁂※✕✓₫֏▣◭∜∯ὄϰ
