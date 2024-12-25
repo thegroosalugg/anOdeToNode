3ab122461c92ecb7f5b9d00968f57f2759e6cad9
victor loginov <tkoe88@zoho.com>
2024-12-26 00:58:11 +0100

    replaced windows & alerts with modals

--- 12-Pagination/public/script/deleteItem.js
+++ 12-Pagination/public/script/deleteItem.js
@@ -1,27 +1,21 @@
-const deleteItem = async (btn) => {
-  const confirmed = window.confirm('Are you sure you want to delete this item?');
-
-  if (!confirmed) {
-    return;
-  }
-  // parent HTML required to query siblings. Use [] to find an element via props
-  const csrf   = btn.parentNode.querySelector('[name=_csrf]').value;
-  const itemId = btn.parentNode.querySelector('[name=itemId]').value;
+const deleteItem = async (csrf, itemId) => {
+  document.querySelector('.modal-confirm').close();
 
   try {
     const response = await fetch('/admin/item/' + itemId, {
       method: 'DELETE',
       headers: {
         'x-csrf-token': csrf,
-        'Accept': 'application/json' // indicates its a fetch request
+              'Accept': 'application/json' // indicates its a fetch request
       }
     });
 
     if (!response.ok) {
-      return window.alert('Looks like an error occured!')
+      document.querySelector('.modal-error').showModal();
+      return;
     }
-    // delete LI from DOM
-    btn.parentNode.parentNode.remove();
+
+    document.getElementById(itemId).remove();
   } catch (error) {
     window.alert(error.message);
   }
