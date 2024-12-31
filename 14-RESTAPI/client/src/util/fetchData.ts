fbdc6fcc1556d936280e3276abd7dcd4176474eb
victor loginov <tkoe88@zoho.com>
2024-12-31 01:47:37 +0100

    update fetch function to handle json & multipart formdata

--- 14-RESTAPI/client/src/util/fetchData.ts
+++ 14-RESTAPI/client/src/util/fetchData.ts
@@ -1,19 +1,19 @@
 export interface Fetch {
       url: string;
   method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    data?: Record<string, string>;
 }
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
-const fetchData = async ({ url, method = 'GET' }: Fetch) => {
-  const headers: HeadersInit = { 'Content-Type': 'application/json' };
+const fetchData = async ({ url, method = 'GET', data }: Fetch) => {
+  const isFile = data instanceof FormData; // multipart/form-data
+  const   body = data ? (isFile ? data : JSON.stringify(data)) : null;
+  const headers: HeadersInit = {};
+  if (!isFile) headers['Content-Type'] = 'application/json';
 
-  const response = await fetch(BASE_URL + url, {
-    method,
-    headers,
-  });
-
-  const resData = await response.json();
+  const response = await fetch(BASE_URL + url, { method, headers, body });
+  const resData  = await response.json();
 
   if (!response.ok) {
     throw { ...resData, status: response.status };
