8c8e8993b81a48d511b465802292a5196a72fc23
victor loginov <tkoe88@zoho.com>
2025-01-03 19:21:19 +0100

    handle image uploads on backend

--- 14-RESTAPI/server/controllers/feedController.ts
+++ 14-RESTAPI/server/controllers/feedController.ts
@@ -1,7 +1,8 @@
 import { RequestHandler } from 'express';
+import { unlink } from 'fs';
 import Post from '../models/Post';
-import errorMsg from '../util/errorMsg';
 import { getErrors, hasErrors } from '../validation/validators';
+import errorMsg from '../util/errorMsg';
 
 const getPosts: RequestHandler = async (req, res, next) => {
   try {
@@ -19,14 +20,17 @@ const getPosts: RequestHandler = async (req, res, next) => {
 const newPost: RequestHandler = async (req, res, next) => {
   try {
     const { title, content } = req.body;
+    const image = req.file;
 
     const errors = getErrors(req);
     if (hasErrors(errors)) {
       res.status(422).json(errors);
+      if (image) unlink(image.path, (error) => errorMsg({ error, where: 'FS Unlink' }));
       return;
     }
 
     const post = new Post({ title, content, user: '67768c6bfa4804119c44db20'});
+    if (image) post.imgURL = image.path;
     await post.save();
     res.status(201).json(post);
   } catch (error) {
