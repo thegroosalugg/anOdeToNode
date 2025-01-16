d15c0502edcafd330b3e82eddc9516e291926e5d
victor loginov <tkoe88@zoho.com>
2025-01-16 20:06:27 +0100

    styled reply page

--- 14-RESTAPI/client/src/components/post/Replies.tsx
+++ 14-RESTAPI/client/src/components/post/Replies.tsx
@@ -1,11 +1,23 @@
 import Reply from '@/models/Reply';
 import css from './Replies.module.css';
+import { timeAgo } from '@/util/timeStamps';
+import ProfilePic from '../profile/ProfilePic';
 
 export default function Replies({ replies }: { replies: Reply[] }) {
   return (
     <ul className={css['replies']}>
       {replies.map(({ _id, content, updatedAt, creator }) => (
-        <li key={_id}>{content}</li>
+        <li key={_id}>
+          <h2>
+            <ProfilePic user={creator} />
+            <span>
+              {creator?.name    || 'Account '}
+              {creator?.surname || 'deleted'}
+            </span>
+            <time>{timeAgo(updatedAt)}</time>
+          </h2>
+          <p>{content}</p>
+        </li>
       ))}
     </ul>
   );
