a7ed3d3405084d899ea08017d43ba595c7cdd57b
victor loginov <tkoe88@zoho.com>
2025-02-24 14:07:53 +0100

    log function updated on client

--- 14-RESTAPI/client/src/util/captainsLog.ts
+++ 14-RESTAPI/client/src/util/captainsLog.ts
@@ -7,7 +7,8 @@ const getHSL = (hue: number) => {
 };
 
 export const captainsLog = ([hue1, hue2]: [number, number], data: unknown[]) => {
-  const time = new Date().toLocaleTimeString([], {
+  const stack = new Error().stack?.split("\n")[2].match(/\/([^/]+)\.tsx?/i)?.[1] || 'Unknown';
+  const  time = new Date().toLocaleTimeString([], {
       hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
@@ -18,12 +19,12 @@ export const captainsLog = ([hue1, hue2]: [number, number], data: unknown[]) => 
   data.forEach((item) => {
     const type = Array.isArray(item) ? 'Array' : typeof item;
     if (typeof item === 'object' && item !== null) {
-      console.groupCollapsed(`%c${type} [${time}]:`, style);
+      console.groupCollapsed(`%c${type} [${time}] ▶️${stack}`, style);
       if (Array.isArray(item)) console.table(item);
       else                       console.dir(item);
       console.groupEnd();
     } else {
-      console.log(`%c' ${item} [${time}]`, style);
+      console.log(`%c' ${item} [${time}] ▶️${stack}`, style);
     }
   });
 };
