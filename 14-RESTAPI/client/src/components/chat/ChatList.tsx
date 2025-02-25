9e8004a7726a50210e0f65b9c826df247c2d0303
victor loginov <tkoe88@zoho.com>
2025-02-25 22:00:23 +0100

    added chatlist fallback

--- 14-RESTAPI/client/src/components/chat/ChatList.tsx
+++ 14-RESTAPI/client/src/components/chat/ChatList.tsx
@@ -147,94 +147,100 @@ export default function ChatList({
                 </AnimatePresence>
               </section>
             )}
-            <AnimatePresence>
-              {(isActive ?? chats).map((chat, i) => {
-                const { _id, host, guest, lastMsg, alerts } = chat;
-                const   recipient = user._id === host._id ? guest : host;
-                const      sender = lastMsg?.sender === user._id ? 'Me' : recipient.name;
-                const         url = `chat/new-msg/${recipient._id}`;
-                const        path = `/inbox/${recipient._id}`;
-                const    isMarked = toBeDeleted[chat._id];
-                const           x = 20 * (i % 2 === 0 ? 1 : -1);
-                const borderColor = isMarked ? '#ffffff00' : 'var(--team-green)';
-                const  background = isMarked
-                  ? 'linear-gradient(to right, #c65740, #ce4429)'
-                  : `var(--${isMenu ? 'main' : 'box'}-gradient)`;
+            <AnimatePresence mode='popLayout'>
+              {!chats.length && !isActive ? (
+                <motion.p key='fallback' className={css['fallback']} {...animations}>
+                  You haven't started any chats
+                </motion.p>
+              ) : (
+                (isActive ?? chats).map((chat, i) => {
+                  const { _id, host, guest, lastMsg, alerts } = chat;
+                  const   recipient = user._id === host._id ? guest : host;
+                  const      sender = lastMsg?.sender === user._id ? 'Me' : recipient.name;
+                  const         url = `chat/new-msg/${recipient._id}`;
+                  const        path = `/inbox/${recipient._id}`;
+                  const    isMarked = toBeDeleted[chat._id];
+                  const           x = 20 * (i % 2 === 0 ? 1 : -1);
+                  const borderColor = isMarked ? '#ffffff00' : 'var(--team-green)';
+                  const  background = isMarked
+                    ? 'linear-gradient(to right, #c65740, #ce4429)'
+                    : `var(--${isMenu ? 'main' : 'box'}-gradient)`;
 
-                return (
-                  <motion.li
-                      layout
-                         key={_id}
-                     onClick={() => setDeletedOrActive(chat, path)}
-                       style={{ cursor }}
-                        exit={{  opacity,    flex, x }}
-                    variants={{
-                       hidden: { opacity,    flex, x },
-                      visible: { opacity: 1, flex, x: 0, background, borderColor },
-                    }}
-                    transition={transition}
-                  >
-                    <h2>
-                      <ProfilePic
-                            layout
-                           animate={{ borderColor }}
-                        transition={transition}
-                              user={recipient}
-                      />
-                      <Span layout {...{ color: '#000', isMarked }}>
-                        {recipient.name} {recipient.surname}
-                      </Span>
-                      <AnimatePresence mode='wait'>
-                        {isActive ? (
-                          <Button
-                             layout
-                                key='btn'
-                                hsl={[180, 80, 35]}
-                               exit={{ opacity }}
-                            onClick={collapse}
-                          >
-                            Back
-                          </Button>
-                        ) : (
-                          <motion.section layout key={lastMsg.updatedAt} {...animations}>
-                            <Span {...{ color: 'var(--text-grey)', isMarked }}>
-                              {timeAgo(lastMsg.updatedAt)}
-                            </Span>
-                            <span>
-                              <Span {...{ color: 'var(--team-green)', isMarked }}>
-                                {'🗨️' + sender}
+                  return (
+                    <motion.li
+                         layout
+                            key={_id}
+                        onClick={() => setDeletedOrActive(chat, path)}
+                          style={{ cursor }}
+                           exit={{ opacity,     flex, x }}
+                       variants={{
+                          hidden: { opacity,    flex, x },
+                         visible: { opacity: 1, flex, x: 0, background, borderColor },
+                       }}
+                      transition={transition}
+                    >
+                      <h2>
+                        <ProfilePic
+                              layout
+                             animate={{ borderColor }}
+                                user={recipient}
+                          transition={transition}
+                        />
+                        <Span layout {...{ color: '#000', isMarked }}>
+                          {recipient.name} {recipient.surname}
+                        </Span>
+                        <AnimatePresence mode='wait'>
+                          {isActive ? (
+                            <Button
+                               layout
+                                  key='btn'
+                                  hsl={[180, 80, 35]}
+                                 exit={{ opacity }}
+                              onClick={collapse}
+                            >
+                              Back
+                            </Button>
+                          ) : (
+                            <motion.section layout key={lastMsg.updatedAt} {...animations}>
+                              <Span   {...{ color: 'var(--text-grey)',  isMarked }}>
+                                {timeAgo(lastMsg.updatedAt)}
                               </Span>
-                              <Counter count={alerts[user._id]} />
-                            </span>
-                            <Span {...{ color: 'var(--dark-grey)', isMarked }}>
-                              {lastMsg.content}
-                            </Span>
-                          </motion.section>
+                              <span>
+                                <Span {...{ color: 'var(--team-green)', isMarked }}>
+                                  {'🗨️' + sender}
+                                </Span>
+                                <Counter count={alerts[user._id]} />
+                              </span>
+                              <Span   {...{ color: 'var(--dark-grey)',  isMarked }}>
+                                {lastMsg.content}
+                              </Span>
+                            </motion.section>
+                          )}
+                        </AnimatePresence>
+                      </h2>
+                      <AnimatePresence>
+                        {isActive && (
+                          <>
+                            <Messages
+                              {...{
+                                       user,
+                                     isMenu,
+                                       chat,
+                                   setChats,
+                                  hasLoaded,
+                                   msgState,
+                                    setMsgs,
+                                clearAlerts,
+                              }}
+                            />
+                            <SendMessage {...{ setUser, isMenu, url }} />
+                          </>
                         )}
                       </AnimatePresence>
-                    </h2>
-                    <AnimatePresence>
-                      {isActive && (
-                        <>
-                          <Messages
-                            {...{
-                                     user,
-                                   isMenu,
-                                     chat,
-                                 setChats,
-                                hasLoaded,
-                                 msgState,
-                                  setMsgs,
-                              clearAlerts,
-                            }}
-                          />
-                          <SendMessage {...{ setUser, isMenu, url }} />
-                        </>
-                      )}
-                    </AnimatePresence>
-                  </motion.li>
-                );
-              })}
+                    </motion.li>
+                  );
+                })
+              )}
             </AnimatePresence>
           </motion.ul>
         </LayoutGroup>
