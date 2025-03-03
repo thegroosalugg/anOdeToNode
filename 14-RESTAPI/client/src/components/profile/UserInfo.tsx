1a56d6a9c7e7be09def67d7c10c92c9cac56bfbf
victor loginov <tkoe88@zoho.com>
2025-03-03 23:46:12 +0100

    userInfo FC functions and animations updated

--- 14-RESTAPI/client/src/components/profile/UserInfo.tsx
+++ 14-RESTAPI/client/src/components/profile/UserInfo.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { motion } from 'motion/react';
+import { motion, useAnimate } from 'motion/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import useDebounce from '@/hooks/useDebounce';
 import User from '@/models/User';
 import Button from '../button/Button';
 import css from './UserInfo.module.css';
@@ -14,26 +15,64 @@ const icons = {
 
 function InfoField({ id, text }: { id: keyof typeof icons; text: string }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [scope,          animate] = useAnimate();
+  const { deferring,    deferFn } = useDebounce();
+  const width = isEditing ? 30 : 60;
+
+  const animation = () =>
+    animate('p, section', { opacity: [null, 0, 1] }, { duration: 0.8, ease: 'easeInOut' });
+
+  const editAction = (callback: () => void) => {
+    deferFn(() => {
+      animation();
+      setTimeout(() => callback(), 400);
+    }, 1000)
+  }
+
+  function saveOrEdit() {
+    editAction(() => {
+      if (isEditing) {
+        console.log('saved');
+        setIsEditing(false);
+      } else {
+        setIsEditing(true);
+      }
+    });
+  }
+
+  function cancel() {
+    editAction(() => setIsEditing(false));
+  }
+
 
   return (
-    <div className={css['info-field']}>
+    <div className={css['info-field']} ref={scope}>
       <h2>
         <FontAwesomeIcon icon={icons[id]} />
         {id}
       </h2>
-      <motion.p
-            key={isEditing + ''}
-        animate={{ opacity: [0, 1], transition: { duration: 1, ease: 'easeInOut' } }}
-      >
+      <motion.p>
         {isEditing ? (
           <input {...{ id, name: id }} placeholder='type here' autoComplete='off' />
         ) : (
           <span>{text}</span>
         )}
       </motion.p>
-      <Button hsl={[0, 0, 100]} onClick={() => setIsEditing(!isEditing)}>
-        Add
-      </Button>
+      <motion.section className={css['buttons']}>
+        <Button
+              hsl={[0, 0, 100]}
+          onClick={saveOrEdit}
+            style={{ width }}
+         disabled={deferring}
+        >
+          {isEditing ? <FontAwesomeIcon icon='check' /> : 'Add'}
+        </Button>
+        {isEditing && (
+          <Button hsl={[0, 0, 100]} onClick={cancel} disabled={deferring}>
+            <FontAwesomeIcon icon='x' />
+          </Button>
+        )}
+      </motion.section>
     </div>
   );
 }
