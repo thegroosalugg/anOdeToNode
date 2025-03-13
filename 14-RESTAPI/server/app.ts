886f0d5a38ccd6059a0bcc0eea75482cbdf51a69
victor loginov <tkoe88@zoho.com>
2025-03-13 18:33:44 +0100

    added credentials

--- 14-RESTAPI/server/app.ts
+++ 14-RESTAPI/server/app.ts
@@ -49,6 +49,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   // sets allowed headers, content-type for req body
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
