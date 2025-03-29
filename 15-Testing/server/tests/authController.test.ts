687149395f7334bc850085c0151d602ed46bd52f
victor loginov <tkoe88@zoho.com>
2025-03-29 23:28:12 +0100

    added login controller tests

--- 15-Testing/server/tests/authController.test.ts
+++ 15-Testing/server/tests/authController.test.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import AppError from '../models/Error';
-import { postSignup } from '../controllers/authController';
+import { postLogin, postSignup } from '../controllers/authController';
 import { getBack, mockReq, settle } from '../util/testHelpers';
 
 jest.mock('../models/User');
@@ -13,9 +13,13 @@ jest.mock('../socket', () => ({
   })),
 }));
 
-jest.mock('bcryptjs', () => ({
-  hash: jest.fn().mockResolvedValue('hashed'),
-}));
+jest.mock('bcryptjs', () => {
+  const { settle } = jest.requireActual('../util/testHelpers');
+  return {
+       hash: settle('hashed'),
+    compare: settle(true),
+  };
+});
 
 jest.mock('jsonwebtoken', () => ({
   sign: jest.fn().mockReturnValue('token'),
@@ -24,21 +28,26 @@ jest.mock('jsonwebtoken', () => ({
 const JWTaccess = 'token', JWTrefresh = JWTaccess;
 
 const signUpTest =
-  'should hash password, save user, sign 2 tokens, return 201, return JSON with 5 props';
+  'should hash password, save user, sign 2 tokens, status(201), return JSON user';
+
+const loginTest =
+  'should compare password, sign 2 tokens, status(200), return JSON user';
 
 describe('Auth Controllers', () => {
+  beforeEach(() => jest.clearAllMocks()); // resets all mocks & instances before each test
+
+  const     name = 'John';
+  const  surname = 'Pliskin';
+  const    email = 'hudson@river.com';
+  const password = '2yearsAgo';
+  const     user = new User({ name, surname, email, password });
+
   it(signUpTest, async () => {
-    const      name = 'John';
-    const   surname = 'Pliskin';
-    const     email = 'a@b.com';
-    const  password = '123qwe';
     const defineReq = { body: { name, surname, email, password } };
     const { req, res, next } = mockReq(defineReq);
 
-    const user = new User({ name, surname, email, password });
     User.prototype.save     = settle(user); // settle/getBack reduce long jest boilerplate
     User.prototype.toObject = getBack({ name, surname, email });
-
     await postSignup(req, res, next);
 
     expect(bcrypt.hash).toHaveBeenCalledWith(password, 12);
@@ -50,5 +59,25 @@ describe('Auth Controllers', () => {
     );
   });
 
-  // it('', () => {});
+  it(loginTest, async () => {
+    const defineReq = { body: { email, password } };
+    const { req, res, next } = mockReq(defineReq);
+    const hashed = 'hashed';
+
+    (User.findOne as jest.Mock).mockResolvedValue({
+       ...user,
+      password: hashed,
+      populate: jest.fn().mockResolvedValue(user),
+      toObject: jest.fn(() => ({ name, surname, email })),
+    });
+
+    await postLogin(req, res, next);
+
+    expect(bcrypt.compare).toHaveBeenCalledWith(password, hashed);
+    expect(jwt.sign).toHaveBeenCalledTimes(2);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({ JWTaccess, JWTrefresh, name, surname, email })
+    );
+  });
 });
