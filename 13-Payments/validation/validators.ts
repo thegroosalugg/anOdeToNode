d2f8512d792395f09ccfcde0328e8546a22d06b1
victor loginov <tkoe88@zoho.com>
2025-08-02 12:40:06 +0200

    overhauled auth form

--- 13-Payments/validation/validators.ts
+++ 13-Payments/validation/validators.ts
@@ -1,6 +1,8 @@
 import { body, check, FieldValidationError, validationResult } from 'express-validator';
 import { Request } from 'express';
 
+const msg = (length: number) => `requires a minimum of ${length} characters.`;
+
 export const getErrors = (req: Request) =>
   validationResult(req)
     .array() // exp-val 7 no longer has union type errors. Must type guard for FieldErrors
@@ -13,7 +15,7 @@ export const hasErrors = (obj: Object) => Object.keys(obj).length > 0;
 export const validateName = body('name')
   .trim()
   .isLength({ min: 3 })
-  .withMessage('needs 3+ chars')
+  .withMessage(msg(3))
   .customSanitizer((value) => value.replace(/<|>/g, '')); // escapes only dangerous values
 
 export const validateEmail = check('email')
@@ -23,7 +25,7 @@ export const validateEmail = check('email')
 
 export const validatePassword = body('password')
   .isLength({ min: 6 })
-  .withMessage('needs 6+ chars')
+  .withMessage(msg(6))
   .custom((value, { req }) => {
     if (value !== req.body.confirm_password) {
       throw new Error("doesn't match");
@@ -33,13 +35,13 @@ export const validatePassword = body('password')
 
 export const validateDesc = body('desc')
   .trim()
-  .isLength({ min: 30 })
-  .withMessage('needs 30+ chars')
+  .isLength({ min: 10 })
+  .withMessage(msg(10))
   .customSanitizer((value) => value.replace(/<|>/g, ''));
 
 export const validatePrice = body('price')
   .isFloat({ min: 0 })
-  .withMessage('must be numeric');
+  .withMessage('must be a number.');
 
 export const validateSignUp = [validateName, validateEmail, validatePassword];
 export const   validateItem = [validateName, validateDesc,  validatePrice   ];
