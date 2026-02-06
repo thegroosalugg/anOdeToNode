b13fa66a85921848d0354e7eefc9a6af538705db
victor loginov <tkoe88@zoho.com>
2026-02-06 10:00:03 +0100

    footer updated

--- 14-RESTAPI/client/src/components/layout/footer/Footer.tsx
+++ 14-RESTAPI/client/src/components/layout/footer/Footer.tsx
@@ -4,23 +4,18 @@ import css from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={css["footer"]}>
-      <hr />
-      <section>
-        <h1>FriendFace</h1>
+      <section className={css["top-section"]}>
+        <p>FriendFace</p>
         <nav>
-          <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/terms">Terms & Conditions</Link>
+          <Link to="/privacy">Privacy Policy</Link>
         </nav>
-        <h1>M.E.R.N.</h1>
+        <p>M.E.R.N.</p>
       </section>
-      <hr />
-      <section>
+      <section className={css["bottom-section"]}>
         <p>© 2025 Social Media Demo. All rights reserved.</p>
-        <a
-            href="https://github.com/thegroosalugg/anOdeToNode/tree/main/14-RESTAPI"
-          target="_blank"
-        >
+        <a href="https://github.com/thegroosalugg/anOdeToNode/tree/main/14-RESTAPI" target="_blank">
           View Source Code
         </a>
         <p>Designed with React 18, Express 4, Mongoose 8 & Socket.IO</p>
