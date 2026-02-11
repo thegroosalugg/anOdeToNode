7fa17bbbc6583365deaeabfb6b239eed21992872
victor loginov <tkoe88@zoho.com>
2026-02-11 18:00:19 +0100

    exported routes & paths to files

---
+++ 14-RESTAPI/client/src/routes/paths.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+     home: "/",
+     feed: "/feed",
+   postId: "/post/:postId",
+   social: "/social",
+   userId: "/user/:userId",
+    about: "/about",
+    terms: "/terms",
+  privacy: "/privacy",
+    error: "*",
+} as const;
