6583d7cc7963daa7e20ef8c315f34ee92026342e
victor loginov <tkoe88@zoho.com>
2026-02-11 08:04:05 +0100

    disabled test page more

--- 14-RESTAPI/client/src/pages/TestPage.tsx
+++ 14-RESTAPI/client/src/pages/TestPage.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable */
-import { useFetch } from "@/lib/hooks/useFetch";
-import { api } from "@/lib/http/endpoints";
-import { useEffect } from "react";
+// import { useFetch } from "@/lib/hooks/useFetch";
+// import { api } from "@/lib/http/endpoints";
+// import { useEffect } from "react";
 
-export default function TestPage() {
-  const { reqData, isInitial } = useFetch();
+// export default function TestPage() {
+//   const { reqData, isInitial } = useFetch();
 
-  useEffect(() => {
-    reqData({ url: api.feed.posts });
-  }, [reqData]);
+//   useEffect(() => {
+//     reqData({ url: api.feed.posts });
+//   }, [reqData]);
 
-  return null;
-}
+//   return null;
+// }
